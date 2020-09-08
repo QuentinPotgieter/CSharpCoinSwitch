a2fcee486f17d56f964dec93adee276212bbbdc2
QuentinPotgieter <quentinpotgieter@hotmail.co.za>
2020-09-08 16:31:49 +0200

    no message

---
+++ HeadsOrTails/HeadsOrTailsForm.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace HeadsOrTails
+{
+    partial class HeadsOrTailsForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.headsPictureBox = new System.Windows.Forms.PictureBox();
+            this.tailsPictureBox = new System.Windows.Forms.PictureBox();
+            this.showHeadsButton = new System.Windows.Forms.Button();
+            this.showTailsButton = new System.Windows.Forms.Button();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.resetButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.headsPictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tailsPictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // headsPictureBox
+            // 
+            this.headsPictureBox.Image = global::HeadsOrTails.Properties.Resources.Heads1;
+            this.headsPictureBox.Location = new System.Drawing.Point(12, 12);
+            this.headsPictureBox.Name = "headsPictureBox";
+            this.headsPictureBox.Size = new System.Drawing.Size(191, 184);
+            this.headsPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.headsPictureBox.TabIndex = 0;
+            this.headsPictureBox.TabStop = false;
+            this.headsPictureBox.Visible = false;
+            // 
+            // tailsPictureBox
+            // 
+            this.tailsPictureBox.Image = global::HeadsOrTails.Properties.Resources.Tails1;
+            this.tailsPictureBox.Location = new System.Drawing.Point(279, 12);
+            this.tailsPictureBox.Name = "tailsPictureBox";
+            this.tailsPictureBox.Size = new System.Drawing.Size(191, 184);
+            this.tailsPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.tailsPictureBox.TabIndex = 0;
+            this.tailsPictureBox.TabStop = false;
+            this.tailsPictureBox.Visible = false;
+            // 
+            // showHeadsButton
+            // 
+            this.showHeadsButton.Location = new System.Drawing.Point(58, 212);
+            this.showHeadsButton.Name = "showHeadsButton";
+            this.showHeadsButton.Size = new System.Drawing.Size(103, 23);
+            this.showHeadsButton.TabIndex = 1;
+            this.showHeadsButton.Text = "Show Heads";
+            this.showHeadsButton.UseVisualStyleBackColor = true;
+            this.showHeadsButton.Click += new System.EventHandler(this.showHeadsButton_Click);
+            // 
+            // showTailsButton
+            // 
+            this.showTailsButton.Location = new System.Drawing.Point(326, 212);
+            this.showTailsButton.Name = "showTailsButton";
+            this.showTailsButton.Size = new System.Drawing.Size(103, 23);
+            this.showTailsButton.TabIndex = 1;
+            this.showTailsButton.Text = "Show Tails";
+            this.showTailsButton.UseVisualStyleBackColor = true;
+            this.showTailsButton.Click += new System.EventHandler(this.showTailsButton_Click);
+            // 
+            // exitButton
+            // 
+            this.exitButton.Location = new System.Drawing.Point(188, 245);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(103, 23);
+            this.exitButton.TabIndex = 1;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // resetButton
+            // 
+            this.resetButton.Location = new System.Drawing.Point(188, 212);
+            this.resetButton.Name = "resetButton";
+            this.resetButton.Size = new System.Drawing.Size(103, 23);
+            this.resetButton.TabIndex = 1;
+            this.resetButton.Text = "Reset";
+            this.resetButton.UseVisualStyleBackColor = true;
+            this.resetButton.Click += new System.EventHandler(this.resetButton_Click);
+            // 
+            // HeadsOrTailsForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(482, 280);
+            this.Controls.Add(this.resetButton);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.showTailsButton);
+            this.Controls.Add(this.showHeadsButton);
+            this.Controls.Add(this.tailsPictureBox);
+            this.Controls.Add(this.headsPictureBox);
+            this.Name = "HeadsOrTailsForm";
+            this.Text = "Heads or Tails";
+            ((System.ComponentModel.ISupportInitialize)(this.headsPictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tailsPictureBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox headsPictureBox;
+        private System.Windows.Forms.PictureBox tailsPictureBox;
+        private System.Windows.Forms.Button showHeadsButton;
+        private System.Windows.Forms.Button showTailsButton;
+        private System.Windows.Forms.Button exitButton;
+        private System.Windows.Forms.Button resetButton;
+    }
+}
+
---
+++ HeadsOrTails/HeadsOrTailsForm.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HeadsOrTails
+{
+    public partial class HeadsOrTailsForm : Form
+    {
+        public HeadsOrTailsForm()
+        {
+            InitializeComponent();
+        }
+
+        // Click events to toggle visibilities
+        private void showHeadsButton_Click(object sender, EventArgs e)
+        {
+            headsPictureBox.Visible = true;
+            tailsPictureBox.Visible = false;
+        }
+
+        private void showTailsButton_Click(object sender, EventArgs e)
+        {
+            tailsPictureBox.Visible = true;
+            headsPictureBox.Visible = false;
+        }
+
+        // Closes the application
+        private void resetButton_Click(object sender, EventArgs e)
+        {
+            tailsPictureBox.Visible = false;
+            headsPictureBox.Visible = false;
+        }
+
+        // Closes the application
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
